fed267d7960ac08ae9be2250ecd83eed6245e1a5
huguesjohnson <hugues@huguesjohnson.com>
2021-11-28 18:13:07 -0600

    Project restructuring

--- build-asm-only.sh
+++ build-asm-only.sh
@@ -1,11 +1,5 @@
-if [ ! -e "./build-tools/bin/com/huguesjohnson/retailclerk/build/MainBuild.class" ]; then
-	mkdir -p ./build-tools/bin/com/huguesjohnson/retailclerk/build/parameters/
-	javac ./build-tools/src/com/huguesjohnson/PathResolver.java
-	mv ./build-tools/src/com/huguesjohnson/PathResolver.class ./build-tools/bin/com/huguesjohnson/
-	javac ./build-tools/src/com/huguesjohnson/retailclerk/build/parameters/*.java
-	mv ./build-tools/src/com/huguesjohnson/retailclerk/build/parameters/*.class ./build-tools/bin/com/huguesjohnson/retailclerk/build/parameters/
-	javac ./build-tools/src/com/huguesjohnson/retailclerk/build/*.java -cp ./build-tools/bin/:./build-tools/lib/gson-2.8.5.jar
-	mv ./build-tools/src/com/huguesjohnson/retailclerk/build/*.class ./build-tools/bin/com/huguesjohnson/retailclerk/build/
-fi 
-java -cp ./build-tools/bin/:./build-tools/lib/gson-2.8.5.jar com.huguesjohnson.retailclerk.build.MainBuild build-asm-only.json
-
+###########################################################
+# build only asm code
+###########################################################
+echo 'running main build..'
+java -jar ./build-tools/RCBuildTools-20211128.jar build-asm-only.json
--- build.sh
+++ build.sh
@@ -1,55 +1,6 @@
-echo 'Compiling build tools..'
-###########################################################
-# create outout directory and all sub paths needed
-###########################################################
-mkdir -p ./build-tools/bin/com/huguesjohnson/retailclerk/build/objects/
-mkdir -p ./build-tools/bin/com/huguesjohnson/retailclerk/build/parameters/
-
-###########################################################
-# compile PathResolver 
-###########################################################
-javac ./build-tools/src/com/huguesjohnson/PathResolver.java
-
-###########################################################
-# copy compiled class
-###########################################################
-mv ./build-tools/src/com/huguesjohnson/PathResolver.class ./build-tools/bin/com/huguesjohnson/
-
-###########################################################
-# build the various object classes
-###########################################################
-javac -cp ./build-tools/bin/ ./build-tools/src/com/huguesjohnson/retailclerk/build/objects/*.java
-
-###########################################################
-# copy the complied classes
-###########################################################
-mv ./build-tools/src/com/huguesjohnson/retailclerk/build/objects/*.class ./build-tools/bin/com/huguesjohnson/retailclerk/build/objects/
-
-###########################################################
-# build the various build parameters
-###########################################################
-javac -cp ./build-tools/bin/ ./build-tools/src/com/huguesjohnson/retailclerk/build/parameters/*.java
-
-###########################################################
-# copy the complied classes
-###########################################################
-mv ./build-tools/src/com/huguesjohnson/retailclerk/build/parameters/*.class ./build-tools/bin/com/huguesjohnson/retailclerk/build/parameters/
-
-###########################################################
-# compile the main build tool classes
-###########################################################
-javac -cp ./build-tools/bin/:./build-tools/lib/gson-2.8.5.jar ./build-tools/src/com/huguesjohnson/retailclerk/build/*.java
-
-###########################################################
-# copy the complied classes
-###########################################################
-mv ./build-tools/src/com/huguesjohnson/retailclerk/build/*.class ./build-tools/bin/com/huguesjohnson/retailclerk/build/
-
-echo ''
-
 ###########################################################
 # main build
 ###########################################################
-echo 'Running main build..'
-java -cp ./build-tools/bin/:./build-tools/lib/gson-2.8.5.jar com.huguesjohnson.retailclerk.build.MainBuild build.json
+echo 'running main build..'
+java -jar ./build-tools/RCBuildTools-20211128.jar build.json
 

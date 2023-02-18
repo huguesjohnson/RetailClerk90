d9239e4eeb40819a9eba235cc76994b96a1350ca
huguesjohnson <hugues@huguesjohnson.com>
2023-02-18 16:45:03 -0600

    Re-skinning the graphics to use the same tileset as Speedrun Tower.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # main build
 ###########################################################
 echo 'running main build..'
-java -jar ./build-tools/RCBuildTools-20211128.jar build.json
+java -jar ./build-tools/RCBuildTools-20220329.jar build.json

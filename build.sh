965b925e9350c0af6193d2f6e0c9ba18e33d05e6
huguesjohnson <hugues@huguesjohnson.com>
2024-05-05 10:38:08 -0500

    Finishing ending scenes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # main build
 ###########################################################
 echo 'running main build..'
-java -jar ./build-tools/RCBuildTools-20240423.jar build.json
+java -jar ./build-tools/RCBuildTools-20240504.jar build.json
